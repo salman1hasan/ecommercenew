817740d6758679e7919e0964eb59d7af45a82872
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-09-19 12:16:02 -0400

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react"
+import Container from "./Container"
+import Logo from "./Logo"
 
 const Header =() =>{
-  return <div>Header</div>;
-}
+  return ( 
+  <div className="bg-bodyColor h-20">
+   <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
+      <Logo/>
+      <div>
+        <input type='text' placeholder="Search for products" className="placeholder:text-sm flex-1 outline-none"/>
+      </div>
+    </Container>
+   </div>
+   );
+};
 
 export default Header;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from 'react'
+
+const Logo =()=>{
+  return (
+    <Link href={"/"}>
+    <h3 className="text-xl font-semibold hover:text-orange-500 cursor-pointer duration-200">smart.</h3>
+    </Link>
+  )
+}
+
+export default Logo
